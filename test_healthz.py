32c045f6c40c40b8d550eeff4b9976489670415e
forum-bhatt <90527629+forum-bhatt@users.noreply.github.com>
2022-03-05 14:58:23 -0500

    Assignment4

--- test_healthz.py
+++ test_healthz.py
@@ -2,12 +2,10 @@ import pytest
 
 from app import app
 
-
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
-
 
 def test_health(client):
     response = client.get('/healthz')
