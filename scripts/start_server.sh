f71da01e0aa236c7ed4d935b3adcc4816a5b1f39
forum-bhatt <90527629+forum-bhatt@users.noreply.github.com>
2022-04-01 05:50:50 -0400

    remove dist folder

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo tar xvzf /home/ec2-user/dist/webservice-0.0.1.tar.gz -C /home/ec2-user/
+sudo tar xvzf /home/ec2-user/webservice-0.0.1.tar.gz -C /home/ec2-user/
 cd /home/ec2-user/webservice-0.0.1
 # sudo pip3 install -e .
 pip3 install -r /home/ec2-user/requirements.txt
