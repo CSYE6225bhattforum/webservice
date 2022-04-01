9081221e23188f62c206131b0a914802e24b6886
forum-bhatt <90527629+forum-bhatt@users.noreply.github.com>
2022-04-01 04:11:45 -0400

    changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,7 @@ sudo tar xvzf /home/ec2-user/dist/webservice-0.0.1.tar.gz -C /home/ec2-user/
 cd /home/ec2-user/webservice-0.0.1
 # sudo pip3 install -e .
 pip3 install -r /home/ec2-user/requirements.txt
-sudo systemctl start flaskapp.service
+sudo systemctl daemon-reload
+sudo systemctl enable webservice.service
+sudo systemctl start webservice.service
+sudo systemctl status webservice.service
