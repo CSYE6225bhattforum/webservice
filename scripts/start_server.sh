03b43af9a63b36231f83f1e92258fbb7f14addf1
forum-bhatt <90527629+forum-bhatt@users.noreply.github.com>
2022-04-02 11:37:13 -0400

    codedeploy file changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 sudo tar xvzf /home/ec2-user/webservice-0.0.1.tar.gz -C /home/ec2-user/
 cd /home/ec2-user/webservice-0.0.1
-# sudo pip3 install -e .
 pip3 install -r /home/ec2-user/requirements.txt
 sudo systemctl daemon-reload
 sudo systemctl enable webservice.service
