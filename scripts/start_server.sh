30b3d2aeb40b5d4f40daaf6eaecf15bbd0f1ef77
forum-bhatt <90527629+forum-bhatt@users.noreply.github.com>
2022-04-03 17:40:14 -0400

    test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo tar -xvzf --overwrite /home/ec2-user/webservice-0.0.1.tar.gz -C /home/ec2-user/
+#sudo tar -xvz --overwrite /home/ec2-user/webservice-0.0.1.tar.gz -C /home/ec2-user/
+sudo tar xvzf /home/ec2-user/webservice-0.0.1.tar.gz -C /home/ec2-user/
 cd /home/ec2-user/webservice-0.0.1
 pip3 install -r /home/ec2-user/requirements.txt
