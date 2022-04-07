91703dbcc7a9c3b786fce39ca98dfb6522d94f8e
forum-bhatt <90527629+forum-bhatt@users.noreply.github.com>
2022-04-07 13:50:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ import logging
 import sys
 
 #logging.basicConfig(filename='C:/Users/foram/OneDrive/Desktop/csye6225.log', encoding='utf-8', level=logging.INFO)
-logging.basicConfig(filename='/home/ec2-user/csye6225.log', encoding='utf-8', level=logging.INFO)
+logging.basicConfig(filename='/home/ec2-user/csye6225.log', level=logging.INFO)
 
 
 app = Flask(__name__)
